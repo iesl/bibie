1f4daa0433fd4c352f40c7c7b0374a4d9ecce2aa
k8si <ksilvers@cs.umass.edu>
2015-03-13 12:02:40 -0400

    fix train/test scripts (jarfile, mainclass, explicit path to lexicons)

--- scripts/test-model.sh
+++ scripts/test-model.sh
@@ -1,6 +1,11 @@
-testfile="umass-citation/testing.docs"
-modelUrl="file://citationCRF.factorie"
-memSize="40G"
-
-java -Xmx${memSize} -cp target/bibie-1.0-SNAPSHOT-jar-with-dependencies.jar \
-bibie.TestCitationModel --test-file=$testfile --model-url=$modelUrl
+if [ "$BIBROOT" = "" ]
+then
+    echo "set BIBROOT environment variable first"
+else
+    testfile="umass-citation/testing.docs"
+    modelUrl="file://citationCRF.factorie"
+    memSize="40G"
+    lexiconPath="file://$BIBROOT/src/main/resources/lexicons"
+    java -Xmx${memSize} -cp target/bibie-0.1-SNAPSHOT-jar-with-dependencies.jar \
+    edu.umass.cs.iesl.bibie.TestCitationModel --test-file=$testfile --model-url=$modelUrl --lexicons=$lexiconPath
+fi
--- scripts/train-model.sh
+++ scripts/train-model.sh
@@ -1,6 +1,11 @@
-trainfile="umass-citation/training.docs"
-testfile="umass-citation/testing.docs"
-memSize="40G"
-
-java -Xmx${memSize} -cp target/bibie-1.0-SNAPSHOT-jar-with-dependencies.jar \
-bibie.TrainCitationModel --train-file=$trainfile --test-file=$testfile
+if [ "$BIBROOT" = "" ]
+then
+    echo "set BIBROOT environment variable first"
+else
+    trainfile="umass-citation/training.docs"
+    testfile="umass-citation/testing.docs"
+    memSize="40G"
+    lexiconPath="file://$BIBROOT/src/main/resources/lexicons"
+    java -Xmx${memSize} -cp target/bibie-0.1-SNAPSHOT-jar-with-dependencies.jar \
+    edu.umass.cs.iesl.bibie.TrainCitationModel --train-file=$trainfile --test-file=$testfile --lexicons=$lexiconPath
+fi
