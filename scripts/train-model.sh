6cbda4b6b64bfdc29c2e7f8faac5f07e1f31d57f
k8si <ksilvers@umass.edu>
2015-05-13 12:38:46 -0400

    make lexicons path explicit

--- scripts/train-model.sh
+++ scripts/train-model.sh
@@ -1,6 +1,12 @@
-trainfile="umass-citation/training.docs"
-testfile="umass-citation/testing.docs"
-memSize="40G"
+#!/bin/bash
 
-java -Xmx${memSize} -cp target/bibie-0.1-SNAPSHOT-jar-with-dependencies.jar \
-edu.umass.cs.iesl.bibie.TrainCitationModel --train-file=$trainfile --test-file=$testfile
+if [ -z "$BIBROOT" ]; then
+    echo "set BIBROOT environment variable"
+else
+    trainfile="umass-citation/training.docs"
+    testfile="umass-citation/testing.docs"
+    lexicons="file://$BIBROOT/src/main/resources/lexicons"
+    memSize="40G"
+    java -Xmx${memSize} -cp target/bibie-0.1-SNAPSHOT-jar-with-dependencies.jar \
+    edu.umass.cs.iesl.bibie.TrainCitationModel --train-file=$trainfile --test-file=$testfile --lexicons=$lexicons
+fi
