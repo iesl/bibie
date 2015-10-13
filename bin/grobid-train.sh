5a00368b866037ff76585022892b3f1b2670b7e8
k8si <ksilvers@cs.umass.edu>
2015-10-13 16:13:59 -0400

    update training scripts to reflect changes

---
+++ bin/grobid-train.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+mem="16G"
+
+dataDir="/iesl/canvas/ksilvers/bibie-exec/data-grobid-24-Sept"
+
+fac="factorie_2.11-1.2-SNAPSHOT-nlp-jar-with-dependencies.jar"
+jarfile="bibie-0.1-SNAPSHOT.jar"
+
+cp="$fac:$jarfile"
+
+java -Xmx$mem -cp $cp edu.umass.cs.iesl.bibie.model.CitationTaggerTrainer \
+--tagger-type="grobid" \
+--optimizer="lbfgs" \
+--save-model="true" \
+--model-file="bibie.grobid.factorie" \
+--train-file="$dataDir/citation4654903728793743412.train" \
+--dev-file="$dataDir/citation4679257556954193486.test" \
+--lexicons="file:///iesl/canvas/ksilvers/bibie-exec/src/main/resources/lexicons"
+
+
